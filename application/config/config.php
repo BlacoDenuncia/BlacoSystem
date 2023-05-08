be9aa65ad1461668ea0dd84d43e7de75ec2a1d7a
Pedro Silva <pedrildojw@gmail.com>
2023-05-08 18:54:38 -0300

    Remove ci index page and set encription key

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost/blaco/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+/*$config['index_page'] = 'index.php';*/
 
 /*
 |--------------------------------------------------------------------------
@@ -326,7 +326,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/userguide3/libraries/encryption.html
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'B14C0çoiuytrfddvfbbhgfds';
 
 /*
 |--------------------------------------------------------------------------
