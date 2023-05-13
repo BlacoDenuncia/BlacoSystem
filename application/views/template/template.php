4aebb108bddd39b8ffb35f8854d7bc06a51149a0
Pedro Silva <pedrildojw@gmail.com>
2023-05-12 21:50:56 -0300

    Adicionar template ao projeto
    A biblioteca template foi adicionada para facilitar reutilização de código

---
+++ application/views/template/template.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blaco - Denuncie</title>
+</head>
+<body>
+    <!--
+        1 - classe responsiva principal
+        2 - content
+        3 - menu
+        Fazer estrutura normal completa, depois separar e substituir por echo no local
+    -->
+</body>
+</html>
