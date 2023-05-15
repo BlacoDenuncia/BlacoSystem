d24b99a984201b9a92df11c09d22a688700b3442
Pedro Silva <pedrildojw@gmail.com>
2023-05-15 19:27:13 -0300

    Adicionar funcionalidades ao menu e estiliza-lo
    Funcionalidade adicionadas:
    - Ao clicar mudar a cor
    - Links aos controllers
    - Icones com tamanhos melhorados

--- application/views/usuarios/menu_user.php
+++ application/views/usuarios/menu_user.php
@@ -1,25 +1,37 @@
 <nav class="bottom-nav container-fluid">
 	<div class="bottom-nav-item">
 		<div class="bottom-nav-item-content">
-        <i class="bi bi-house"></i>
-			one
+			<a class="link-dark" href="Index_controller">
+        		<i class="bi bi-house"></i>
+			</a>
 		</div>		
 	</div>
 	<div class="bottom-nav-item">		
 		<div class="bottom-nav-item-content">
-            <i class="fas fa-house"></i>
-			two
+			<a class="link-dark" href="tel:197">
+            	<i class="bi bi-telephone"></i>
+			</a>
 		</div>
 	</div>
 	<div class="bottom-nav-item">
 		<div class="bottom-nav-item-content">
-			<i class="">person</i>
+			<a class="link-dark" href="Boletim_controller">
+				<i class="bi bi-exclamation-octagon"></i>
+			</a>
 		</div>		
 	</div>
-	
 	<div class="bottom-nav-item">
 		<div class="bottom-nav-item-content">
-			<i class="">phone</i>
+			<a class="link-dark" href="Conteudo_controller">
+				<i class="bi bi-book-half"></i>
+			</a>
+		</div>		
+	</div>
+	<div class="bottom-nav-item">
+		<div class="bottom-nav-item-content">
+			<a class="link-dark" href="Conta_controller">
+				<i class="bi bi-person-circle"></i>
+			</a>
 		</div>		
 	</div>
 </nav>
--- utils/js/custom_script.js
+++ utils/js/custom_script.js
@@ -1,9 +1,16 @@
-var navItems = document.querySelectorAll(".mobile-bottom-nav__item");
-navItems.forEach(function(e, i) {
-	e.addEventListener("click", function(e) {
-		navItems.forEach(function(e2, i2) {
-			e2.classList.remove("mobile-bottom-nav__item--active");
-		})
-		this.classList.add("mobile-bottom-nav__item--active");
-	});
+// Mudar a cor dos itens do menu ao serem selcecionados
+var bottomNavItems = document.querySelectorAll('.bottom-nav-item'); 
+    var activeItem = null;
+
+    bottomNavItems.forEach(function(item) {
+      item.addEventListener('click', function() {
+        var icon = this.querySelector('i');
+        
+        if (activeItem !== null) {
+          activeItem.querySelector('i').classList.remove('red');
+        }
+
+        icon.classList.add('red');
+        activeItem = this;
+      });
 });
