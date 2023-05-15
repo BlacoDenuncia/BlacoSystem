34f9163e0ea57924ebf3b98b7f9c701a769e63db
Pedro Silva <pedrildojw@gmail.com>
2023-05-14 21:04:05 -0300

    Configura load básico de template e implementa base do menu_user
    - Adiciona Index_controller e define como principal
    - Adiciona Bootstrap, Bootstrap Icons, Fontawesome
    - Adiciona estrutura basica do menu já responsivo
    
    Próximas atividades:
    - Corrigir icones e tamanho dos icones do menu
    - Linkar o menu com seus respectivos controllers
    - Adicionar função JS que ao clicar em um item do menu, muda a cor dele
    - Personalizaar cores dos menus

--- application/views/template/template.php
+++ application/views/template/template.php
@@ -4,6 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Load custom styles --> 
+    <link rel="stylesheet" href="<?php echo base_url();?>utils/styles/custom_style.scss">
+    <link rel="stylesheet" href="<?php echo base_url();?>utils/styles/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>utils/styles/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <!-- Load external JavaScript -->
+    <script src="<?php echo base_url();?>utils/js/bootstrap.bundle.min.js"></script>
+    
     <title>Blaco - Denuncie</title>
 </head>
 <body>
@@ -13,5 +22,14 @@
         3 - menu
         Fazer estrutura normal completa, depois separar e substituir por echo no local
     -->
+    <div class="container-fluid">
+        <?php 
+            echo $content;    
+        ?>
+        <?php
+            echo $menu;
+        ?>
+    </div>
+    <script src="<?php echo base_url();?>utils/js/custom_script.js"></script>
 </body>
 </html>
---
+++ application/views/usuarios/menu_user.php
@@ -0,0 +1,25 @@
+<nav class="bottom-nav container-fluid">
+	<div class="bottom-nav-item">
+		<div class="bottom-nav-item-content">
+        <i class="bi bi-house"></i>
+			one
+		</div>		
+	</div>
+	<div class="bottom-nav-item">		
+		<div class="bottom-nav-item-content">
+            <i class="fas fa-house"></i>
+			two
+		</div>
+	</div>
+	<div class="bottom-nav-item">
+		<div class="bottom-nav-item-content">
+			<i class="">person</i>
+		</div>		
+	</div>
+	
+	<div class="bottom-nav-item">
+		<div class="bottom-nav-item-content">
+			<i class="">phone</i>
+		</div>		
+	</div>
+</nav>
