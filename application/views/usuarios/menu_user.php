3566171d319da307849c9876f354ea728d0ba0a0
Pedro Silva <pedrildojw@gmail.com>
2023-05-19 18:40:42 -0300

    Change class of links if the ternary operator returns true

--- application/views/usuarios/menu_user.php
+++ application/views/usuarios/menu_user.php
@@ -1,7 +1,7 @@
 <nav class="bottom-nav container-fluid">
 	<div class="bottom-nav-item">
 		<div class="bottom-nav-item-content">
-			<a class="link-dark" href="Index_controller">
+			<a class="link-dark <?php echo ($current_page === 'index') ? 'active' : ''; ?>" href="Index_controller">
         		<i class="bi bi-house"></i>
 			</a>
 		</div>		
@@ -15,21 +15,21 @@
 	</div>
 	<div class="bottom-nav-item">
 		<div class="bottom-nav-item-content">
-			<a class="link-dark" href="Boletim_controller">
+			<a class="link-dark <?php echo ($current_page === 'boletim') ? 'active' : ''; ?>" href="Boletim_controller">
 				<i class="bi bi-exclamation-octagon"></i>
 			</a>
 		</div>		
 	</div>
 	<div class="bottom-nav-item">
 		<div class="bottom-nav-item-content">
-			<a class="link-dark" href="Conteudo_controller">
+			<a class="link-dark <?php echo ($current_page === 'conteudo') ? 'active' : ''; ?>" href="Conteudo_controller">
 				<i class="bi bi-book-half"></i>
 			</a>
 		</div>		
 	</div>
 	<div class="bottom-nav-item">
 		<div class="bottom-nav-item-content">
-			<a class="link-dark" href="Conta_controller">
+			<a class="link-dark <?php echo ($current_page === 'conta') ? 'active' : ''; ?>" href="Conta_controller">
 				<i class="bi bi-person-circle"></i>
 			</a>
 		</div>		
