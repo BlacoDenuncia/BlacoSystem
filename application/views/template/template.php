bbc6b20fbf45768a9aa876dcd21b9537561bf55d
Pedro Silva <pedrildojw@gmail.com>
2023-06-08 16:43:51 -0300

    Check the connection with internet
    Add js and jQuery checks to user connection. If is not connected show a
    modal that leave try again or go to didatic page. The modal isn't stylized
    
    - in js you can add mora offline pages in future

--- application/views/template/template.php
+++ application/views/template/template.php
@@ -29,11 +29,34 @@
         <?php
             echo $menu;
         ?>
+    <!--modal de conxão com internet-->
+    <div id="modalDesconectado" class="modal fade" tabindex="-1" role="dialog">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <i class="bi bi-wifi-off"></i>
+                    <h5 class="modal-title">Sem conexão com a internet</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+            <div class="modal-body">
+                <p>Por favor verifique sua conexão com a internet e tente novamente. 
+                    Enquanto isso, que tal acessar a <a href="Conteudo_controller"> área didática?</a></p>
+            </div>
+            <div class="modal-footer">
+                <button id="btnChecaInternet"type="button" class="btn btn-secondary" data-bs-dismiss="modal">TENTAR NOVAMENTE</button>
+            </div>
+        </div>
     </div>
+</div>
+    
+    </div>
+
+    
 
     <!--importa scripts após carregamento da página-->
     <script src="<?php echo base_url();?>utils/js/jquery.min.js"></script>
     <script src="<?php echo base_url();?>utils/js/custom_script.js"></script>
     
+
 </body>
 </html>
--- utils/js/custom_script.js
+++ utils/js/custom_script.js
@@ -1,5 +1,67 @@
-//Função para expandir cards de conteudos
+//checa internet antes do carregamento total da página
+function checkInternetConnection() {
+    /*$.ajax({
+      url: 'https://blaco-teste.000webhostapp.com/', // Replace with your own URL or API endpoint
+      dataType: 'jsonp',
+      timeout: 10000, // Adjust timeout value as needed
+      success: function() {
+        console.log('Connected to the internet');
+        // Perform additional actions or enable online functionality
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        console.log('Not connected to the internet');
+        // Display the modal
+        $('#myModal').modal('show');
+      }
+    });*/
+    if (navigator.onLine) {
+        console.log('Connected to the internet');
+        // Perform additional actions or enable online functionality
+      } else {
+        console.log('Not connected to the internet');
+        // Display the modal
+        $('#modalDesconectado').modal('show');
+    }
+}
+
+
 $(document).ready(function () {
+    // Recebe a URL da página atual
+    var currentPageUrl = window.location.href;
+
+    // Define as URLs das páginas que não precisam de acesso à internet
+    var offlinePages = [
+    'https://blaco-teste.000webhostapp.com/Conteudo_controller'
+  ];
+
+  // Checa se a página atual é uma das páginas do array
+  var isOfflinePage = offlinePages.includes(currentPageUrl);
+
+  // Se a página não é offline checa a conexão com a internet
+  if (!isOfflinePage) {
+    checkInternetConnection();
+  }
+
+    // Listen for online and offline events
+    $(window).on('online', function() {
+        console.log('Connected to the internet');
+    // Perform additional actions or enable online functionality
+    });
+
+    $(window).on('offline', function() {
+        if (!isOfflinePage) {
+            console.log('Not connected to the internet');
+            // Display the modal
+            $('#modalDesconectado').modal('show');
+          }
+        
+    });
+
+    $('#btnChecaInternet').click(function() {
+        checkInternetConnection();
+    });
+
+//Função para expandir cards de conteudos
     $(".conteudos-title").click(function (e) {
         // Recebe o valor do atributo data-tab do elemento clicado
         var conteudositem = $(this).attr("data-tab");
