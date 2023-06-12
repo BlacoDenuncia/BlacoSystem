1031fc68cdb1dcbd32553654c373dcea4ef7aad7
Pedro Silva <pedrildojw@gmail.com>
2023-06-12 18:11:01 -0300

    Add new labels and organize

--- application/views/usuarios/boletim/boletim_view.php
+++ application/views/usuarios/boletim/boletim_view.php
@@ -9,38 +9,69 @@
             <label class="form-label" for="nome">Nome</label>
             <input type="text" class="form-control" id="nome" name="nome" placeholder="Digite seu nome" required>
          </div>
-         <div class="form-group col-md-3">
+         <div class="form-group col-md-1">
             <label class="form-label" for="idade">Idade</label>
             <input type="number" class="form-control" id="idade" name="idade" required>
          </div>
-         <div class="form-group col-md-4">
+         <div class="form-group col-md-3">
             <label class="form-label" for="numContato">Telefone de contato</label>
-            <input type="text" class="form-control" id="numContato" name="numContato" placeholder="Digite o seu número" required>
+            <input type="text" class="form-control" id="numContato" name="numContato" placeholder="Digite o seu número"
+               required>
+         </div>
+         <div class="form-group col-md-3">
+            <label class="form-label" for="email">Email de contato</label>
+            <input type="email" class="form-control" id="email" name="email" placeholder="Digite seu melhor email"
+               required>
          </div>
       </div>
       <div class="row">
-         <div class="form-group col-md-6">
-            <label class="form-label" for="email">Email de contato</label>
-            <input type="email" class="form-control" id="email" name="email" placeholder="Digite seu melhor email" required>
-         </div>
-         <div class="form-group col-md-6">
-            <label class="form-label" for="tipoViolencia">Tipo de violência</label>
-            <select class="form-control" id="tipoViolencia" name="tipoViolencia">
-               <option value="1º Ano">A</option>
-               <option value="2º Ano">B</option>
-               <option value="3º Ano">C</option>
+
+         <div class="form-group col-md-3">
+            <label class="form-label" for="generoVitima">Selecione seu gênero</label>
+            <select class="form-control" id="generoVitima" name="generoVitima">
+               <option value="A">A</option>
+               <option value="B">B</option>
+               <option value="C">C</option>
             </select>
          </div>
+         <div class="form-group col-md-3">
+            <label class="form-label" for="etniaVitima">Etnia/Raça</label>
+            <select class="form-control" id="etniaVitima" name="etniaVitima">
+               <option value="A">A</option>
+               <option value="B">B</option>
+               <option value="C">C</option>
+            </select>
+         </div>
+         <div class="form-group col-md-3">
+            <label class="form-label" for="tipoViolencia">Tipo de violência</label>
+            <select class="form-control" id="tipoViolencia" name="tipoViolencia">
+               <option value="A">A</option>
+               <option value="B">B</option>
+               <option value="C">C</option>
+            </select>
+         </div>
+         <div class="form-group col-md-3">
+            <label class="form-label" for="profissaoVitima">Qual sua profissão?</label>
+            <input type="profissaoVitima" class="form-control" id="profissaoVitima" name="profissaoVitima"
+               placeholder="" required>
+         </div>
       </div>
       <div class="row">
-         <div class="form-group">
-            <label class="form-label" for="descricao">Descrição</label>
-            <textarea class="form-control" id="descricao" name="descricao" rows="5" placeholder="Descreva com detalhes o agressor e o que aconteceu" required></textarea>
+         <div class="form-group col-md-6">
+            <label class="form-label" for="descricaoAgressor">Descreva o agressor</label>
+            <textarea class="form-control" id="descricaoAgressor" name="descricaoAgressor" rows="5"
+               placeholder="Descreva com detalhes o agressor" required></textarea>
+         </div>
+         <div class="form-group col-md-6">
+            <label class="form-label" for="descricaoCaso">Descreva o que ocorreu</label>
+            <textarea class="form-control" id="descricaoCaso" name="descricaoCaso" rows="5"
+               placeholder="Descreva o que aconteceu" required></textarea>
          </div>
       </div>
       <div class="row">
          <div class="form-group form-button col-md-2">
-            <button type="button" class="btn btn-danger btn-sm btnRecebeLocalizacao" id="btnRecebeLocalizacao"><i class="bi bi-geo-alt-fill"></i>Localização atual</button>
+            <button type="button" class="btn btn-danger btn-sm btnRecebeLocalizacao" id="btnRecebeLocalizacao"><i
+                  class="bi bi-geo-alt-fill"></i>Localização atual</button>
          </div>
          <div class="form-group col-md-2">
             <label class="form-label" for="bairro">Bairro</label>
@@ -48,11 +79,13 @@
          </div>
          <div class="form-group col-md-3">
             <label class="form-label" for="cidade">Cidade</label>
-            <input type="text" class="form-control" id="cidade" name="cidade" placeholder="Cidade em que ocorreu" required>
+            <input type="text" class="form-control" id="cidade" name="cidade" placeholder="Cidade em que ocorreu"
+               required>
          </div>
          <div class="form-group col-md-2">
             <label class="form-label" for="estado">Estado</label>
-            <input type="text" class="form-control" id="estado" name="estado" placeholder="Estado em que ocorreu" required>
+            <input type="text" class="form-control" id="estado" name="estado" placeholder="Estado em que ocorreu"
+               required>
          </div>
          <div class="form-group col-md-3">
             <label class="form-label" for="rua">Rua</label>
@@ -62,7 +95,8 @@
       <div class="row">
          <div class="form-group col-md-2">
             <label class="form-label" for="numeroDoLocal">Número do local</label>
-            <input type="text" class="form-control" id="numeroDoLocal" name="numeroDoLocal" placeholder="Número do estabelecimento" required>
+            <input type="text" class="form-control" id="numeroDoLocal" name="numeroDoLocal"
+               placeholder="Número do estabelecimento" required>
          </div>
          <div class="form-group col-md-4">
             <label class="form-label" for="tipoEstabelecimento">Tipo do estabelecimento</label>
@@ -77,52 +111,63 @@
             </select>
          </div>
          <div class="form-group col-md-4">
+            <label class="form-label" for="nomeEstabelecimento">Nome do estabelecimento</label>
+            <input type="text" class="form-control" id="nomeEstabelecimento" name="nomeEstabelecimento"
+               placeholder="Exemplo: Nome da empressa, nome do comércio" required>
+         </div>
+         
+      </div>
+      <div class="row" >
+      <div class="form-group col-md-2">
             <label class="form-label" for="temTestemunhas">Houveram testemunhas?</label>
             <div class="form-check">
                <input type="checkbox" class="form-check-input" id="temTestemunhas" name="temTestemunhas">
-               <label class="form-check-label" for="temTestemunhas">Yes</label>
+               <label class="form-check-label" for="temTestemunhas">Sim</label>
             </div>
          </div>
-      </div>
-      <div class="row" id="camposTestemunhas">
-         <div class="form-group col-md-3">
-            <label class="form-label" for="nomeTestemunha">Witness Name</label>
-            <input type="text" class="form-control" id="nomeTestemunha" name="nomeTestemunha">
-         </div>
-         <div class="form-group col-md-3">
-            <label class="form-label" for="numeroTestemunha">Contato da testemunha</label>
-            <input type="text" class="form-control" id="numeroTestemunha" name="numeroTestemunha">
-         </div>
-         <div class="form-group col-md-3">
-            <label class="form-label" for="emailTestemunha">Email da testemunha</label>
-            <input type="email" class="form-control" id="emailTestemunha" name="emailTestemunha">
+         <div class=" row form-group col-md-10" id="camposTestemunhas">
+            <div class="form-group col-md-3">
+               <label class="form-label" for="nomeTestemunha">Witness Name</label>
+               <input type="text" class="form-control" id="nomeTestemunha" name="nomeTestemunha">
+            </div>
+            <div class="form-group col-md-3">
+               <label class="form-label" for="numeroTestemunha">Contato da testemunha</label>
+               <input type="text" class="form-control" id="numeroTestemunha" name="numeroTestemunha">
+            </div>
+            <div class="form-group col-md-3">
+               <label class="form-label" for="emailTestemunha">Email da testemunha</label>
+               <input type="email" class="form-control" id="emailTestemunha" name="emailTestemunha">
+            </div>
          </div>
       </div>
       <div class="row">
          <div class="form-group col-md-6">
             <label class="form-label" for="acoesTomadas">Ações tomadas</label>
-            <textarea class="form-control" id="acoesTomadas" name="acoesTomadas" placeholder="O que você fez logo após o ocorrido?" rows="5"></textarea>
+            <textarea class="form-control" id="acoesTomadas" name="acoesTomadas"
+               placeholder="O que você fez logo após o ocorrido?" rows="5"></textarea>
          </div>
          <div class="form-group col-md-6">
-            <label class="form-label" for="impactoEmocional">Como se sente?</label>
-            <textarea class="form-control" id="impactoEmocional" name="impactoEmocional" placeholder="Tente encontrar uma única palavra, se possível." rows="5"></textarea>
+            <label class="form-label" for="impactoEmocional">Como se sentiu?</label>
+            <textarea class="form-control" id="impactoEmocional" name="impactoEmocional"
+               placeholder="Tente encontrar uma única palavra, se possível." rows="5"></textarea>
          </div>
       </div>
       <div class="modal-footer">
          <div class="form-group form-check">
             <input type="checkbox" class="form-check-input" id="aceitaDadosCheck" required>
-            <label class="form-check-label" for="aceitaDadosCheck">Eu aceito a utilização dos meus dados para fins de estatística ( Não divulgaremos seus dados pessoais )</label>
+            <label class="form-check-label" for="aceitaDadosCheck">Eu aceito a utilização dos meus dados para fins de
+               estatística ( Não divulgaremos seus dados pessoais )</label>
          </div>
          <div class="form-button">
             <div class="form-group">
                <button type="button" class="btn btn-success btn-enviar" id="btn-enviar">
-               <span class="bi bi-check" aria-hidden="true"></span> Enviar 
+                  <span class="bi bi-check" aria-hidden="true"></span> Enviar
                </button>
             </div>
             <div class="form-group">
                <button type="reset" class="btn btn-dark btn-limpar" id="btn-limpar">
-               <span class="bi bi-eraser" aria-hidden="true"></span> Limpar 
-               </button>   
+                  <span class="bi bi-eraser" aria-hidden="true"></span> Limpar
+               </button>
             </div>
          </div>
       </div>
