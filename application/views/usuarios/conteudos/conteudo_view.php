76906413a5b229d597444dcdcac51333eb43fa0d
PedroX79 <87133537+pedroD630@users.noreply.github.com>
2023-07-19 08:17:23 -0300

    FIX: Didatic page with a white space before content

--- application/views/usuarios/conteudos/conteudo_view.php
+++ application/views/usuarios/conteudos/conteudo_view.php
@@ -1,43 +1,69 @@
 <!--content-->
+<div class="didatic container-fluid">
+    <div class="page-title container">
+        <h1 class="page-title-header">Lorem Ipsum Dolor </h1>
+        <p class="page-title-text">"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur,
+            adipisci velit..."</p>
+    </div>
 
-<div class="page-title container">
-    <h1 class="page-title-header">Lorem Ipsum Dolor </h1>
-    <p class="page-title-text">"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."</p>
-</div>
-
-<div class="container faq">
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="conteudos">
-                    <div class="conteudos-item">
-                        <div class="conteudos-title" data-tab="item1">
-                            <h3>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..." <i class="bi bi-chevron-down"></i></h3>
+    <div class="container faq">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="conteudos">
+                        <div class="conteudos-item">
+                            <div class="conteudos-title" data-tab="item1">
+                                <h3>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur,
+                                    adipisci velit..." <i class="bi bi-chevron-down"></i></h3>
+                            </div>
+                            <div class="conteudos-content" id="item1">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus semper elementum
+                                    efficitur. Nulla diam dolor, vulputate id ex ut, hendrerit ultrices lectus. Integer
+                                    lacus erat, imperdiet vel venenatis at, accumsan ac massa. Maecenas nec placerat
+                                    nisl. Maecenas euismod eget dolor sit amet gravida. Phasellus vitae leo sit amet
+                                    ligula imperdiet laoreet ut congue metus. Vivamus iaculis pellentesque condimentum.
+                                    Vivamus vehicula ante ipsum, sit amet pharetra elit lobortis ac. Nunc ac suscipit
+                                    risus. Etiam sed libero nunc. Nam ut arcu non est posuere sagittis ac eget tellus.
+                                    Nunc ac lobortis lorem, ac tincidunt quam.
+                                </p>
+                            </div>
                         </div>
-                        <div class="conteudos-content" id="item1">
-                            <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus semper elementum efficitur. Nulla diam dolor, vulputate id ex ut, hendrerit ultrices lectus. Integer lacus erat, imperdiet vel venenatis at, accumsan ac massa. Maecenas nec placerat nisl. Maecenas euismod eget dolor sit amet gravida. Phasellus vitae leo sit amet ligula imperdiet laoreet ut congue metus. Vivamus iaculis pellentesque condimentum. Vivamus vehicula ante ipsum, sit amet pharetra elit lobortis ac. Nunc ac suscipit risus. Etiam sed libero nunc. Nam ut arcu non est posuere sagittis ac eget tellus. Nunc ac lobortis lorem, ac tincidunt quam. 
-                            </p>
+                        <div class="conteudos-item">
+                            <div class="conteudos-title" data-tab="item2">
+                                <h3>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur,
+                                    adipisci velit..." <i class="bi bi-chevron-down"></i></h3>
+                            </div>
+                            <div class="conteudos-content" id="item2">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus semper elementum
+                                    efficitur. Nulla diam dolor, vulputate id ex ut, hendrerit ultrices lectus. Integer
+                                    lacus erat, imperdiet vel venenatis at, accumsan ac massa. Maecenas nec placerat
+                                    nisl. Maecenas euismod eget dolor sit amet gravida. Phasellus vitae leo sit amet
+                                    ligula imperdiet laoreet ut congue metus. Vivamus iaculis pellentesque condimentum.
+                                    Vivamus vehicula ante ipsum, sit amet pharetra elit lobortis ac. Nunc ac suscipit
+                                    risus. Etiam sed libero nunc. Nam ut arcu non est posuere sagittis ac eget tellus.
+                                    Nunc ac lobortis lorem, ac tincidunt quam.
+                                </p>
+                            </div>
                         </div>
-                    </div>
-                    <div class="conteudos-item">
-                        <div class="conteudos-title" data-tab="item2">
-                            <h3>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..." <i class="bi bi-chevron-down"></i></h3>
-                        </div>
-                        <div class="conteudos-content" id="item2">
-                            <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus semper elementum efficitur. Nulla diam dolor, vulputate id ex ut, hendrerit ultrices lectus. Integer lacus erat, imperdiet vel venenatis at, accumsan ac massa. Maecenas nec placerat nisl. Maecenas euismod eget dolor sit amet gravida. Phasellus vitae leo sit amet ligula imperdiet laoreet ut congue metus. Vivamus iaculis pellentesque condimentum. Vivamus vehicula ante ipsum, sit amet pharetra elit lobortis ac. Nunc ac suscipit risus. Etiam sed libero nunc. Nam ut arcu non est posuere sagittis ac eget tellus. Nunc ac lobortis lorem, ac tincidunt quam. 
-                            </p>
-                        </div>
-                    </div>
-                    <div class="conteudos-item">
-                        <div class="conteudos-title" data-tab="item3">
-                            <h3>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..." <i class="bi bi-chevron-down"></i></h3>
-                        </div>
-                        <div class="conteudos-content" id="item3">
-                            <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus semper elementum efficitur. Nulla diam dolor, vulputate id ex ut, hendrerit ultrices lectus. Integer lacus erat, imperdiet vel venenatis at, accumsan ac massa. Maecenas nec placerat nisl. Maecenas euismod eget dolor sit amet gravida. Phasellus vitae leo sit amet ligula imperdiet laoreet ut congue metus. Vivamus iaculis pellentesque condimentum. Vivamus vehicula ante ipsum, sit amet pharetra elit lobortis ac. Nunc ac suscipit risus. Etiam sed libero nunc. Nam ut arcu non est posuere sagittis ac eget tellus. Nunc ac lobortis lorem, ac tincidunt quam. 
-                            </p>
+                        <div class="conteudos-item">
+                            <div class="conteudos-title" data-tab="item3">
+                                <h3>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur,
+                                    adipisci velit..." <i class="bi bi-chevron-down"></i></h3>
+                            </div>
+                            <div class="conteudos-content" id="item3">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus semper elementum
+                                    efficitur. Nulla diam dolor, vulputate id ex ut, hendrerit ultrices lectus. Integer
+                                    lacus erat, imperdiet vel venenatis at, accumsan ac massa. Maecenas nec placerat
+                                    nisl. Maecenas euismod eget dolor sit amet gravida. Phasellus vitae leo sit amet
+                                    ligula imperdiet laoreet ut congue metus. Vivamus iaculis pellentesque condimentum.
+                                    Vivamus vehicula ante ipsum, sit amet pharetra elit lobortis ac. Nunc ac suscipit
+                                    risus. Etiam sed libero nunc. Nam ut arcu non est posuere sagittis ac eget tellus.
+                                    Nunc ac lobortis lorem, ac tincidunt quam.
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
