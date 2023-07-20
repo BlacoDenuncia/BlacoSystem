5f7f15394d8b8fddb33ce3667402aa0cdbdab6ca
PedroX79 <87133537+pedroD630@users.noreply.github.com>
2023-07-20 16:24:24 -0300

    Add a home page

---
+++ application/views/usuarios/index_view.php
@@ -0,0 +1,35 @@
+<div class="title-user-container">
+    <div class="blaco">
+        <img class="blaco-logo" id="blaco-logo" href="">
+        <h1 class="blurred-text">BLACO</h1>
+    </div>
+
+    <i class="bi bi-person-circle"></i>
+</div>
+<div class="home-links">
+    <p class="page-title-text">Texto aqui Texto aqui Texto aqui</p>
+    <div class="row justify-content-center">
+        <div class="col-md-6 medium-div"
+            style="background-image: url('<?php echo base_url(); ?>utils/img/teste.webp');">
+            <div class="text">
+                <a href="Boletim_controller" class="text-white">
+                    Denuncie agora!
+                </a>
+            </div>
+        </div>
+        <div class="col-md-6 medium-div background-1">
+            <div class="text">Veja as delegacias mais próximas!</div>
+        </div>
+    </div>
+
+    <div class="row justify-content-center align-items-center">
+        <div class="col-md-6 medium-div"
+            style="background-image: url('<?php echo base_url(); ?>utils/img/teste.webp');">
+            <div class="text">Ligar para a polícia</div>
+        </div>
+        <div class="col-md-6 medium-div"
+            style="background-image: url('<?php echo base_url(); ?>utils/img/teste.webp');">
+            <div class="text">Aprenda mais!</div>
+        </div>
+    </div>
+</div>
