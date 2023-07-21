01fd63872e7fd21882ee68b52d34df28ead56552
PedroX79 <87133537+pedroD630@users.noreply.github.com>
2023-07-21 15:45:19 -0300

    Finish home page basic style

--- application/views/usuarios/index_view.php
+++ application/views/usuarios/index_view.php
@@ -4,32 +4,47 @@
         <h1 class="blurred-text">BLACO</h1>
     </div>
 
-    <i class="bi bi-person-circle"></i>
+    <a href="Conta_controller" class="text-white user-icon"><i class="bi bi-person-circle user-icon"></i></a>
 </div>
 <div class="home-links">
-    <p class="page-title-text">Texto aqui Texto aqui Texto aqui</p>
+    <div class="row" id="intro-text">
+        <p class="page-title-text">Unidos contra o racismo: <span class="orange-text">denuncie, lute e
+                transforme!</span></p>
+    </div>
     <div class="row justify-content-center">
-        <div class="col-md-6 medium-div"
-            style="background-image: url('<?php echo base_url(); ?>utils/img/teste.webp');">
-            <div class="text">
-                <a href="Boletim_controller" class="text-white">
+        <div class="col-md-6 medium-div">
+            <a href="Boletim_controller" class="text-white">
+                <div class="link-card-text">
                     Denuncie agora!
-                </a>
-            </div>
+                </div>
+            </a>
         </div>
-        <div class="col-md-6 medium-div background-1">
-            <div class="text">Veja as delegacias mais próximas!</div>
+        <div class="col-md-6 medium-div">
+            <a href="tel:197" class="text-white">
+                <div class="link-card-text">
+                    Ligar para policia
+                </div>
+            </a>
         </div>
     </div>
 
     <div class="row justify-content-center align-items-center">
-        <div class="col-md-6 medium-div"
-            style="background-image: url('<?php echo base_url(); ?>utils/img/teste.webp');">
-            <div class="text">Ligar para a polícia</div>
+        <div class="col-md-6 medium-div">
+            <a href="Mapa_controller" class="text-white">
+                <div class="link-card-text">
+                    Mapa de delegacias
+                </div>
+            </a>
         </div>
-        <div class="col-md-6 medium-div"
-            style="background-image: url('<?php echo base_url(); ?>utils/img/teste.webp');">
-            <div class="text">Aprenda mais!</div>
+        <div class="col-md-6 medium-div">
+            <a href="Conteudo_controller" class="text-white">
+                <div class="link-card-text">
+                    Aprenda mais
+                </div>
+            </a>
         </div>
     </div>
+</div>
+<div class="recent-news">
+    <h3 class="blurred-text">Notícias e Atualizações (Em breve) &#8595;</h3>
 </div>
