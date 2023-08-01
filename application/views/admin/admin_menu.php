1f97e97ba65b61dae08f2cb9df966fc20e567b13
PedroX79 <87133537+pedroD630@users.noreply.github.com>
2023-08-01 15:43:46 -0300

    Change menu to active colors

--- application/views/admin/admin_menu.php
+++ application/views/admin/admin_menu.php
@@ -1,7 +1,7 @@
 <nav class="bottom-nav container-fluid">
 	<div class="bottom-nav-item">
 		<div class="bottom-nav-item-content">
-			<a class="link-white <?php echo ($current_page === 'index_admin') ? 'active' : ''; ?>" href="Conta_controller">
+			<a class=" <?php echo ($current_page === 'dashboard') ? 'active' : 'text-white'; ?>" href="Conta_controller">
         		<i class="bi bi-clipboard-data"></i>
 				<p class="menu-text">Dashboard</p>
 			</a>
@@ -9,7 +9,7 @@
 	</div>
 	<div class="bottom-nav-item">
 		<div class="bottom-nav-item-content">
-			<a class="link-white <?php echo ($current_page === 'relatorios') ? 'active' : ''; ?>" href="Relatorios_controller">
+			<a class=" <?php echo ($current_page === 'planilhas') ? 'active' : 'text-white'; ?>" href="Planilhas_controller">
 				<i class="bi bi-pie-chart-fill"></i>
 				<p class="menu-text">Relátórios</p>
 			</a>
@@ -17,7 +17,7 @@
 	</div>
 	<div class="bottom-nav-item">
 		<div class="bottom-nav-item-content">
-			<a class="link-danger <?php echo ($current_page === 'sair') ? 'active' : ''; ?>" id="fazerLogout">
+			<a class="link-danger" id="fazerLogout">
 				<i class="bi bi-box-arrow-right"></i>
 				<p class="menu-text">Sair</p>
 			</a>
--- application/views/admin/dashboard_view.php
+++ application/views/admin/dashboard_view.php
@@ -1,6 +1,6 @@
 
     
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
 </head>
 
 <body>
