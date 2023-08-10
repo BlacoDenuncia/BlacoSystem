653b673fe57bbb73b5d83bf1796afb57850efd40
Pedro Silva <87133537+pedroD630@users.noreply.github.com>
2023-08-10 11:05:13 -0300

    Adiciona conteudo de teste

--- application/views/usuarios/conteudos/conteudo_view.php
+++ application/views/usuarios/conteudos/conteudo_view.php
@@ -10,59 +10,57 @@
         <div class="row">
             <div class="col-sm-12">
                 <div class="conteudos">
+					<!--COPIAR AQUI-->
                     <div class="conteudos-item">
                         <div class="conteudos-title" data-tab="item1">
-                            <h3>"Neque porro " </h3><i class="bi bi-chevron-down"></i>
+                            <h3>Vasco da Gama maior do Rio</h3><i class="bi bi-chevron-down"></i>
                         </div>
                         <div class="conteudos-content" id="item1">
                             <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus semper elementum
-                                efficitur. Nulla diam dolor, vulputate id ex ut, hendrerit ultrices lectus. Integer
-                                lacus erat, imperdiet vel venenatis at, accumsan ac massa. Maecenas nec placerat
-                                nisl. Maecenas euismod eget dolor sit amet gravida. Phasellus vitae leo sit amet
-                                ligula imperdiet laoreet ut congue metus. Vivamus iaculis pellentesque condimentum.
-                                Vivamus vehicula ante ipsum, sit amet pharetra elit lobortis ac. Nunc ac suscipit
-                                risus. Etiam sed libero nunc. Nam ut arcu non est posuere sagittis ac eget tellus.
-                                Nunc ac lobortis lorem, ac tincidunt quam.
+                               Vamos todos cantar de coração
+A Cruz de Malta é o meu pendão
+Tu tens o nome do heroico português
+Vasco da Gama, a tua fama assim se fez
+
+Tua imensa torcida é bem feliz
+Norte-Sul, Norte-Sul deste Brasil
+Tua estrela, na terra a brilhar
+Ilumina o mar
+
+No atletismo, és um braço
+No remo, és imortal
+No futebol, és um traço
+De união Brasil-Portugal
+
+No atletismo, és um braço
+No remo, és imortal
+No futebol, és um traço
+De união Brasil-Portugal
+
+Vamos todos cantar de coração
+A Cruz de Malta é o meu pendão!
+Tu tens o nome do heroico português
+Vasco da Gama, a tua fama assim se fez
+
+Tua imensa torcida é bem feliz
+Norte-Sul, Norte-Sul deste Brasil
+Tua estrela, na terra a brilhar
+Ilumina o mar
+
+No atletismo, és um braço
+No remo, és imortal
+No futebol, és um traço
+De união Brasil-Portugal
+
+No atletismo, és um braço
+No remo, és imortal
+No futebol, és um traço
+De união Brasil-Portugal
                             </p>
                         </div>
                     </div>
-                    <div class="conteudos-item">
-                        <div class="conteudos-title" data-tab="item2">
-                            <h3>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur,
-                                adipisci velit..." </h3><i class="bi bi-chevron-down"></i>
-                        </div>
-                        <div class="conteudos-content" id="item2">
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus semper elementum
-                                efficitur. Nulla diam dolor, vulputate id ex ut, hendrerit ultrices lectus. Integer
-                                lacus erat, imperdiet vel venenatis at, accumsan ac massa. Maecenas nec placerat
-                                nisl. Maecenas euismod eget dolor sit amet gravida. Phasellus vitae leo sit amet
-                                ligula imperdiet laoreet ut congue metus. Vivamus iaculis pellentesque condimentum.
-                                Vivamus vehicula ante ipsum, sit amet pharetra elit lobortis ac. Nunc ac suscipit
-                                risus. Etiam sed libero nunc. Nam ut arcu non est posuere sagittis ac eget tellus.
-                                Nunc ac lobortis lorem, ac tincidunt quam.
-                            </p>
-                        </div>
-                    </div>
-                    <div class="conteudos-item">
-                        <div class="conteudos-title" data-tab="item3">
-                            <h3>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur,
-                                adipisci velit..." </h3><i class="bi bi-chevron-down"></i>
-                        </div>
-                        <div class="conteudos-content" id="item3">
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus semper elementum
-                                efficitur. Nulla diam dolor, vulputate id ex ut, hendrerit ultrices lectus. Integer
-                                lacus erat, imperdiet vel venenatis at, accumsan ac massa. Maecenas nec placerat
-                                nisl. Maecenas euismod eget dolor sit amet gravida. Phasellus vitae leo sit amet
-                                ligula imperdiet laoreet ut congue metus. Vivamus iaculis pellentesque condimentum.
-                                Vivamus vehicula ante ipsum, sit amet pharetra elit lobortis ac. Nunc ac suscipit
-                                risus. Etiam sed libero nunc. Nam ut arcu non est posuere sagittis ac eget tellus.
-                                Nunc ac lobortis lorem, ac tincidunt quam.
-                            </p>
-                        </div>
-                    </div>
+					<!--ACABA-->
+					<!--se precisar copiar o de cima e colar-->
                 </div>
             </div>
         </div>
