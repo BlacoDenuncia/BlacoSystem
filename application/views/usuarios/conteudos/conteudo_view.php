87f976f87c9c446990d7f57c33e570cfa476e0d4
Pedro Silva <87133537+pedroD630@users.noreply.github.com>
2023-08-17 11:00:38 -0300

    Add navs and tabs on content page
    Add navs and tabs for organize contents by type

--- application/views/usuarios/conteudos/conteudo_view.php
+++ application/views/usuarios/conteudos/conteudo_view.php
@@ -1,67 +1,98 @@
 <!--content-->
 <div class="didatic container-fluid">
-    <div class="page-title container">
-        <h1 class="page-title-header">Lorem Ipsum Dolor </h1>
-        <p class="page-title-text">"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur,
-            adipisci velit..."</p>
-    </div>
-
-    <div class="faq">
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="conteudos">
-					<!--COPIAR AQUI-->
-                    <div class="conteudos-item">
+<div class="page-title container">
+   <h1 class="page-title-header"> Página Didática </h1>
+   <p class="page-title-text">"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur,
+      adipisci velit..."
+   </p>
+</div>
+<div class="faq">
+   <div class="row">
+      <div class="col-sm-12">
+         <div class="conteudos">
+            <!---->
+            <div id="tabConteudos" class="container">
+               <ul  class="nav nav-pills">
+                  <li class="">
+                     <a  href="#tabLeis" data-toggle="tab">Leis para casos de racismo</a>
+                  </li>
+                  <li><a href="#tabInfo" data-toggle="tab">Termos imporantes</a>
+                  </li>
+                  <li><a href="#tabHAHA" data-toggle="tab">Teste</a>
+                  </li>
+                  <li><a href="#tabHEHE" data-toggle="tab">Teste</a>
+                  </li>
+               </ul>
+               <div class="tab-content clearfix">
+                  <div class="tab-pane active" id="tabLeis">
+                     <!--conteudo da tab de leis -->
+                     <!--COPIAR AQUI-->
+                     <div class="conteudos-item">
                         <div class="conteudos-title" data-tab="item1">
-                            <h3>Vasco da Gama maior do Rio</h3><i class="bi bi-chevron-down"></i>
+                           <h3>Título</h3>
+                           <i class="bi bi-chevron-down"></i>
                         </div>
                         <div class="conteudos-content" id="item1">
-                            <p>
-                               Vamos todos cantar de coração
-A Cruz de Malta é o meu pendão
-Tu tens o nome do heroico português
-Vasco da Gama, a tua fama assim se fez
-
-Tua imensa torcida é bem feliz
-Norte-Sul, Norte-Sul deste Brasil
-Tua estrela, na terra a brilhar
-Ilumina o mar
-
-No atletismo, és um braço
-No remo, és imortal
-No futebol, és um traço
-De união Brasil-Portugal
-
-No atletismo, és um braço
-No remo, és imortal
-No futebol, és um traço
-De união Brasil-Portugal
-
-Vamos todos cantar de coração
-A Cruz de Malta é o meu pendão!
-Tu tens o nome do heroico português
-Vasco da Gama, a tua fama assim se fez
-
-Tua imensa torcida é bem feliz
-Norte-Sul, Norte-Sul deste Brasil
-Tua estrela, na terra a brilhar
-Ilumina o mar
-
-No atletismo, és um braço
-No remo, és imortal
-No futebol, és um traço
-De união Brasil-Portugal
-
-No atletismo, és um braço
-No remo, és imortal
-No futebol, és um traço
-De união Brasil-Portugal
-                            </p>
+                           <p>
+                              Conteúdo
+                           </p>
                         </div>
-                    </div>
-					<!--ACABA-->
-					<!--se precisar copiar o de cima e colar-->
-                </div>
+                     </div>
+                     <!--ACABA-->
+                     <!--se precisar copiar o de cima e colar-->
+                  </div>
+                  <div class="tab-pane" id="tabInfo">
+                     <!--conteudo da tab de info -->
+                     <!--COPIAR AQUI-->
+                     <div class="conteudos-item">
+                        <div class="conteudos-title" data-tab="item1">
+                           <h3>Título</h3>
+                           <i class="bi bi-chevron-down"></i>
+                        </div>
+                        <div class="conteudos-content" id="item1">
+                           <p>
+                              Conteúdo
+                           </p>
+                        </div>
+                     </div>
+                     <!--ACABA-->
+                     <!--se precisar copiar o de cima e colar-->
+                  </div>
+                  <div class="tab-pane" id="tabHAHA">
+                     <!--COPIAR AQUI-->
+                     <div class="conteudos-item">
+                        <div class="conteudos-title" data-tab="item1">
+                           <h3>Título</h3>
+                           <i class="bi bi-chevron-down"></i>
+                        </div>
+                        <div class="conteudos-content" id="item1">
+                           <p>
+                              Conteúdo
+                           </p>
+                        </div>
+                     </div>
+                     <!--ACABA-->
+                     <!--se precisar copiar o de cima e colar-->
+                  </div>
+                  <div class="tab-pane" id="tabHEHE">
+                     <!--COPIAR AQUI-->
+                     <div class="conteudos-item">
+                        <div class="conteudos-title" data-tab="item1">
+                           <h3>Título</h3>
+                           <i class="bi bi-chevron-down"></i>
+                        </div>
+                        <div class="conteudos-content" id="item1">
+                           <p>
+                              Conteúdo
+                           </p>
+                        </div>
+                     </div>
+                     <!--ACABA-->
+                     <!--se precisar copiar o de cima e colar-->
+                  </div>
+               </div>
             </div>
-        </div>
+         </div>
+      </div>
+   </div>
 </div>
