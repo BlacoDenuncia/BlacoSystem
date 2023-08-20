7121da86e499afa7ad3a9384310c0c15c072dcd0
PedroX79 <87133537+pedroD630@users.noreply.github.com>
2023-08-20 18:16:13 -0300

    Add dinamic navs and content pages

--- application/views/usuarios/conteudos/conteudo_view.php
+++ application/views/usuarios/conteudos/conteudo_view.php
@@ -1,98 +1,54 @@
 <!--content-->
 <div class="didatic container-fluid">
-<div class="page-title container">
-   <h1 class="page-title-header"> Página Didática </h1>
-   <p class="page-title-text">"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur,
-      adipisci velit..."
-   </p>
-</div>
-<div class="faq">
-   <div class="row">
-      <div class="col-sm-12">
-         <div class="conteudos">
-            <!---->
-            <div id="tabConteudos" class="container">
-               <ul  class="nav nav-pills">
-                  <li class="">
-                     <a  href="#tabLeis" data-toggle="tab">Leis para casos de racismo</a>
-                  </li>
-                  <li><a href="#tabInfo" data-toggle="tab">Termos imporantes</a>
-                  </li>
-                  <li><a href="#tabHAHA" data-toggle="tab">Teste</a>
-                  </li>
-                  <li><a href="#tabHEHE" data-toggle="tab">Teste</a>
-                  </li>
-               </ul>
-               <div class="tab-content clearfix">
-                  <div class="tab-pane active" id="tabLeis">
-                     <!--conteudo da tab de leis -->
-                     <!--COPIAR AQUI-->
-                     <div class="conteudos-item">
-                        <div class="conteudos-title" data-tab="item1">
-                           <h3>Título</h3>
-                           <i class="bi bi-chevron-down"></i>
+   <div class="page-title container">
+      <h1 class="page-title-header"> Página Didática </h1>
+      <p class="page-title-text">"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur,
+         adipisci velit..."
+      </p>
+   </div>
+   <div class="faq">
+      <div class="row">
+         <div class="col-sm-12">
+            <div class="conteudos">
+               <!---->
+               <div id="tabConteudos" class="container">
+                  <ul class="nav-tabs nav justify-content-center d-flex" id="myTabs">
+                     <li class="nav-item">
+                        <a href="#tabA" data-toggle="tab" class="nav-link">Itens A</a>
+                     </li>
+                     <li class="nav-item">
+                        <a href="#tabB" data-toggle="tab" class="nav-link">Itens B</a>
+                     </li>
+                     <li class="nav-item">
+                        <a href="#tabC" data-toggle="tab" class="nav-link">Itens C</a>
+                     </li>
+                  </ul>
+                  <div class="tab-content">
+                     <div id="tabA" class="tab-pane container">
+
+                        <!--COPIAR AQUI-->
+                        <div class="conteudos-item">
+                           <div class="conteudos-title" data-tab="item1">
+                              <h3>Título</h3>
+                              <i class="bi bi-chevron-down"></i>
+                           </div>
+                           <div class="conteudos-content" id="item1">
+                              <p>
+                                 Conteúdo
+                              </p>
+                           </div>
                         </div>
-                        <div class="conteudos-content" id="item1">
-                           <p>
-                              Conteúdo
-                           </p>
-                        </div>
+                        <!--ACABA-->
+                        <!--se precisar copiar o de cima e colar-->
+
                      </div>
-                     <!--ACABA-->
-                     <!--se precisar copiar o de cima e colar-->
-                  </div>
-                  <div class="tab-pane" id="tabInfo">
-                     <!--conteudo da tab de info -->
-                     <!--COPIAR AQUI-->
-                     <div class="conteudos-item">
-                        <div class="conteudos-title" data-tab="item1">
-                           <h3>Título</h3>
-                           <i class="bi bi-chevron-down"></i>
-                        </div>
-                        <div class="conteudos-content" id="item1">
-                           <p>
-                              Conteúdo
-                           </p>
-                        </div>
-                     </div>
-                     <!--ACABA-->
-                     <!--se precisar copiar o de cima e colar-->
-                  </div>
-                  <div class="tab-pane" id="tabHAHA">
-                     <!--COPIAR AQUI-->
-                     <div class="conteudos-item">
-                        <div class="conteudos-title" data-tab="item1">
-                           <h3>Título</h3>
-                           <i class="bi bi-chevron-down"></i>
-                        </div>
-                        <div class="conteudos-content" id="item1">
-                           <p>
-                              Conteúdo
-                           </p>
-                        </div>
-                     </div>
-                     <!--ACABA-->
-                     <!--se precisar copiar o de cima e colar-->
-                  </div>
-                  <div class="tab-pane" id="tabHEHE">
-                     <!--COPIAR AQUI-->
-                     <div class="conteudos-item">
-                        <div class="conteudos-title" data-tab="item1">
-                           <h3>Título</h3>
-                           <i class="bi bi-chevron-down"></i>
-                        </div>
-                        <div class="conteudos-content" id="item1">
-                           <p>
-                              Conteúdo
-                           </p>
-                        </div>
-                     </div>
-                     <!--ACABA-->
-                     <!--se precisar copiar o de cima e colar-->
+                     <div id="tabB" class="tab-pane container">Conteúdo sobre cachorros</div>
+                     <div id="tabC" class="tab-pane container">Conteúdo sobre penguins</div>
                   </div>
                </div>
             </div>
          </div>
       </div>
    </div>
+   
 </div>
