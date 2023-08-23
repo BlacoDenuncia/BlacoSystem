afc220e2ea801d8c10f27291eab482c92b355985
PedroX79 <87133537+pedroD630@users.noreply.github.com>
2023-08-23 11:31:00 -0300

    Adiciona conteudos

--- application/views/usuarios/conteudos/conteudo_view.php
+++ application/views/usuarios/conteudos/conteudo_view.php
@@ -40,7 +40,41 @@
                         </div>
                         <!--ACABA-->
                         <!--se precisar copiar o de cima e colar-->
-
+                        <div class="conteudos-item">
+                           <div class="conteudos-title" data-tab="item2A">
+                              <h3>O que é racismo</h3>
+                              <i class="bi bi-chevron-down"></i>
+                           </div>
+                           <div class="conteudos-content" id="item2A">
+                              <p>
+                                 Racismo é a doutrina que afirma superioridade de determinados grupos étnicos,
+                                 nacionais, linguísticos, religiosos, sobre outros. Por extensão, o termo passou
+                                 designar as ideias e práticas discriminatórias
+                                 advindas dessa afirmada superioridade.
+                              </p>
+                              <p>
+                                 O racismo institucional sempre coloca pessoas de grupos raciais ou étnicos
+                                 discriminados em situação de desvantagem no acesso a benefícios gerados pelo Estado por
+                                 demais
+                                 instituições e organizações.
+                              </p>
+                           </div>
+                        </div>
+                        <div class="conteudos-item">
+                           <div class="conteudos-title" data-tab="item3A">
+                              <h3>O que é discriminação racial</h3>
+                              <i class="bi bi-chevron-down"></i>
+                           </div>
+                           <div class="conteudos-content" id="item3A">
+                              <p>
+                                 Qualquer distinção, exclusão, restrição ou preferência baseadas em raça, cor,
+                                 descendência ou origem nacional ou étnica, anulando ou restringindo o reconhecimento,
+                                 gozo ou exercício de direitos
+                                 humanos e liberdades fundamentais. (Artigo 1 - Convenção Internacional sobre a
+                                 Eliminação de todas as Formas de Discriminação Racial)
+                              </p>
+                           </div>
+                        </div>
                      </div>
                      <div id="tabB" class="tab-pane container">
                         <!--COPIAR AQUI-->
