c7afa6c2e928b9cbfa50e6157e9925b534169ff9
PedroX79 <87133537+pedroD630@users.noreply.github.com>
2023-09-04 18:40:46 -0300

    Ajustao botão flutuante
    Neste commit, foram realizadas as seguintes alterações:
    
    - Adiciona um botão flutuante que centraliza na localização atual do usuario
    - O botão flutuante foi ajustado para alinhar à esquerda da tela, alterando as propriedades 'left' e 'right' do '.floating-container'.
    
    Isso garante que o botão seja exibido no canto inferior esquerdo da tela, em vez do canto inferior direito.
    
    Assinado por: [Pedro]

--- application/views/usuarios/mapas/mapa_provisorio.php
+++ application/views/usuarios/mapas/mapa_provisorio.php
@@ -3,4 +3,7 @@
 <script src="<?php echo base_url();?>utils/js/mapa.js"></script>
 
 <div id="map-container"></div>
-<button id="centerButton" class="floating-button">Centralizar</button>
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<div class="floating-container">
+  <div id="centerButton" class="floating-button">+</div>
+</div>
