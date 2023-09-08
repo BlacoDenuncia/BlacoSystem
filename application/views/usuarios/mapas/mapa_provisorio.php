662543c8c5d6d8f98d6a0b6fc7e6b817cf531cdd
PedroX79 <87133537+pedroD630@users.noreply.github.com>
2023-09-07 22:31:18 -0300

    Adiciona estrutura HTML para a barra superior da delegacia mais próxima
    Foi adicionada a estrutura HTML para a barra superior que exibe informações sobre a delegacia mais próxima, incluindo o nome da delegacia e o botão "Ver no Google Maps". Também foi incluído um contêiner para o mapa.

--- application/views/usuarios/mapas/mapa_provisorio.php
+++ application/views/usuarios/mapas/mapa_provisorio.php
@@ -1,6 +1,11 @@
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAILX5C4EmC9jNJrQfi-UhqkudL_TtHVW8&libraries=places"></script>
+<script
+  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAILX5C4EmC9jNJrQfi-UhqkudL_TtHVW8&libraries=places"></script>
 
-<script src="<?php echo base_url();?>utils/js/mapa.js"></script>
+<script src="<?php echo base_url(); ?>utils/js/mapa.js"></script>
+<div id="nearest-police">
+  <p>Delegacia mais próxima: <span id="nearest-name"></span></p>
+  <button id="view-on-maps">Ver no Google Maps</button>
+</div>
 
 <div id="map-container"></div>
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
