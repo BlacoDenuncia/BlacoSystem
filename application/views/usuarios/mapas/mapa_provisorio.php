f5d3115698ebc0710c56413fd7536e0391a3a53c
PedroX79 <87133537+pedroD630@users.noreply.github.com>
2023-09-08 22:04:58 -0300

    Diminui uma linha

--- application/views/usuarios/mapas/mapa_provisorio.php
+++ application/views/usuarios/mapas/mapa_provisorio.php
@@ -1,5 +1,4 @@
-<script
-  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAILX5C4EmC9jNJrQfi-UhqkudL_TtHVW8&libraries=places"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAILX5C4EmC9jNJrQfi-UhqkudL_TtHVW8&libraries=places"></script>
 
 <script src="<?php echo base_url(); ?>utils/js/mapa.js"></script>
 <div id="nearest-police">
