c5f153739d56bef1a1f9e4b037caa09b4c316a5b
PedroX79 <87133537+pedroD630@users.noreply.github.com>
2023-10-06 22:09:31 -0300

    Eliminate render blocking resourses

--- application/views/usuarios/conta/cadastro_view.php
+++ application/views/usuarios/conta/cadastro_view.php
@@ -1,6 +1,7 @@
 <script src="<?php echo base_url(); ?>utils/js/moment.js"></script>
 <link rel="stylesheet" href="<?php echo base_url(); ?>utils/styles/bootstrap-datepicker.min.css">
 <script src="<?php echo base_url(); ?>utils/js/bootstrap-datepicker.min.js"></script>
+<script src="<?php echo base_url(); ?>utils/js/login_user.js"></script>
 
 <main class="d-flex">
     <div class="container d-flex flex-column">
