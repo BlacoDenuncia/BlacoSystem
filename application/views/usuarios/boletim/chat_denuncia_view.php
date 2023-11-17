f9d7b0d4672afeb22e8b4aedff9b12907209c5a2
PedroX79 <87133537+pedroD630@users.noreply.github.com>
2023-11-16 22:23:29 -0300

    Alterar ordem dos elementos e corrigir erros visuais
    Os elementos de resposta do usuario e de mensagem do chat estavam fora do padrão de design

--- application/views/usuarios/boletim/chat_denuncia_view.php
+++ application/views/usuarios/boletim/chat_denuncia_view.php
@@ -28,23 +28,25 @@
             </div>
             <div id="body">
                 <!-- This section will be dynamically inserted from JavaScript -->
-                <div class="userSection">
-                    <div class="messages user-message">
-
-                    </div>
-                    <div class="seperator"></div>
-                </div>
+                <div id="messageValue" hidden></div>
                 <div class="botSection">
                     <div class="messages bot-reply">
 
                     </div>
                     <div class="seperator"></div>
                 </div>
+                <div class="userSection">
+                    <div class="messages user-message">
+
+                    </div>
+                    <div class="seperator"></div>
+                </div>
+                
             </div>
 
             <div id="inputArea">
-                <input type="text" name="messages" id="userInput" placeholder="Please enter your message here" required>
-                <input type="submit" id="send" value="Send">
+                <input type="text" name="messages" id="userInput" placeholder="Responda aqui" required>
+                <button id="send" value="Send" disabled>Enviar</button>
             </div>
         </div>
     </div>
