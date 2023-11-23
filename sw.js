e1b799b307388ef8ab2020e17dcd63eccd07855d
PedroX79 <87133537+pedroD630@users.noreply.github.com>
2023-11-22 22:59:03 -0300

    Alteração para caminhos no localhost ( não implementar )

--- sw.js
+++ sw.js
@@ -6,15 +6,15 @@ self.addEventListener('install', event => {
                 console.log('Cache opened successfully');
                 return cache.addAll([
                     './',
-                    '/utils/js/custom_script.js',
-                    '/utils/js/moment.js',
-                    '/utils/js/jquery.mask.min.js',
-                    '/utils/js/jquery.min.js',
-                    '/utils/js/jquery-ui.min.js',
-                    '/utils/js/bootstrap.bundle.min.js',
-                    '/utils/js/bootstrap-datepicker.min.js',
-                    '/utils/js/boletim.js',
-                    '/utils/js/admin.js',
+                    '/blaco/utils/js/custom_script.js',
+                    '/blaco/utils/js/moment.js',
+                    '/blaco/utils/js/jquery.mask.min.js',
+                    '/blaco/utils/js/jquery.min.js',
+                    '/blaco/utils/js/jquery-ui.min.js',
+                    '/blaco/utils/js/bootstrap.bundle.min.js',
+                    '/blaco/utils/js/bootstrap-datepicker.min.js',
+                    '/blaco/utils/js/boletim.js',
+                    '/blaco/utils/js/admin.js',
                     './conteudo_view.html',
 
                 ]);
@@ -57,6 +57,6 @@ async function networkFirst(request) {
         return networkResponse;
     } catch (err) {
         const cachedResponse = await dynamicCache.match(request);
-        return cachedResponse || await caches.match('/conteudo_view.html');
+        return cachedResponse || await caches.match('/blaco/conteudo_view.html');
     }
 }
