80d843117fd63b8f6d15c1421a724d44f1d4a39f
PedroX79 <87133537+pedroD630@users.noreply.github.com>
2023-12-06 08:55:17 -0300

    Implementar atualização automática de cache
    - Adicionado número de versão constante 'CACHE_VERSION' para facilitar a atualização do cache.
    - No evento 'install', utiliza 'cacheFirst' para armazenar os recursos essenciais no cache durante a instalação.
    - No evento 'activate', exclui caches antigos e garante que o novo Service Worker assume o controle imediatamente usando 'self.clients.claim()'.
    - No evento 'fetch', utiliza estratégias 'cacheFirst' e 'networkFirst' para atender a solicitações com base na origem.
    
    Essas alterações visam melhorar a eficiência do cache e garantir uma transição suave para novas versões da aplicação.

--- sw.js
+++ sw.js
@@ -1,11 +1,23 @@
+const CACHE_VERSION = 'v0';
 
 self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open('blaco-offline-v1')
+        caches.open(CACHE_VERSION)
             .then(cache => {
                 console.log('Cache opened successfully');
                 return cache.addAll([
-                    './',
+                    '/blaco/',
+                    '/blaco/utils/js/custom_script.js',
+                    '/blaco/utils/js/moment.js',
+                    '/blaco/utils/js/jquery.mask.min.js',
+                    '/blaco/utils/js/jquery.min.js',
+                    '/blaco/utils/js/jquery-ui.min.js',
+                    '/blaco/utils/js/bootstrap.bundle.min.js',
+                    '/blaco/utils/js/bootstrap-datepicker.min.js',
+                    '/blaco/utils/js/boletim.js',
+                    '/blaco/utils/js/admin.js',
+                    '/blaco/conteudo_view.html',
+                    /*'./',
                     '/utils/js/custom_script.js',
                     '/utils/js/moment.js',
                     '/utils/js/jquery.mask.min.js',
@@ -15,8 +27,7 @@ self.addEventListener('install', event => {
                     '/utils/js/bootstrap-datepicker.min.js',
                     '/utils/js/boletim.js',
                     '/utils/js/admin.js',
-                    './conteudo_view.html',
-
+                    './conteudo_view.html',*/
                 ]);
             })
             .then(() => {
@@ -26,12 +37,28 @@ self.addEventListener('install', event => {
                 console.error('Cache error:', error);
             })
     );
+    self.skipWaiting();
 });
 
 /*self.addEventListener('install', async function () {
     const cache = await caches.open(blacoCache);
     cache.addAll(staticAssets);
 });*/
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            // Exclua caches antigos (exceto a versão atual)
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_VERSION) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
 
 self.addEventListener('fetch', event => {
     const request = event.request;
@@ -50,13 +77,13 @@ async function cacheFirst(request) {
 
 async function networkFirst(request) {
     const dynamicCache = await caches.open('blaco-dynamic');
-    
+
     try {
         const networkResponse = await fetch(request);
         dynamicCache.put(request, networkResponse.clone());
         return networkResponse;
     } catch (err) {
         const cachedResponse = await dynamicCache.match(request);
-        return cachedResponse || await caches.match('/conteudo_view.html');
+        return cachedResponse || await caches.match('/blaco/conteudo_view.html');
     }
 }
