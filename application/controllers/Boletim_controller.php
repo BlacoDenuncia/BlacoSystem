89add50bd48056e7d18b31b6d9e54ad0a8fff9be
PedroX79 <87133537+pedroD630@users.noreply.github.com>
2023-12-08 12:01:05 -0300

    Adicionar o header no load do template

--- application/controllers/Boletim_controller.php
+++ application/controllers/Boletim_controller.php
@@ -52,10 +52,12 @@ class Boletim_controller extends CI_Controller
                 'current_page' => $this->determineCurrentPage()
             );
 
+            $this->template->write_view('header', 'header_view', $data, FALSE);
             $this->template->write_view('content', 'usuarios/boletim/boletim_view', $data, FALSE);
             $this->template->write_view('menu', 'usuarios/menu_user', $data, FALSE);
             $this->template->render();
         } else {
+            $this->template->write_view('header', 'header_view', $current_page, FALSE);
             $this->template->write_view('content', 'usuarios/boletim/boletim_view', $current_page, FALSE);
             $this->template->write_view('menu', 'usuarios/menu_user', $current_page, FALSE);
             $this->template->render();
