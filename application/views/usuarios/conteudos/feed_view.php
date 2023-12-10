9764cf3ee31ea488345f228a609fcfff13bfe08d
PedroX79 <87133537+pedroD630@users.noreply.github.com>
2023-12-10 16:35:38 -0300

    Mudar referecias do nav de conteudos para tags

---
+++ application/views/usuarios/conteudos/feed_view.php
@@ -0,0 +1,37 @@
+<!--content-->
+<div class="didatic container-fluid">
+    <div class="page-title container">
+        <h1 class="page-title-header"> Página Didática </h1>
+        <p class="page-title-text">Aqui você tem acesso a conceitos e leis existentes no Código Penal, além de direitos
+            para exercer sua cidadania conforme a constituição!</p>
+    </div>
+    <div class="faq">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="conteudos">
+                    <!---->
+                    <div id="tabConteudos" class="container">
+                        <ul class="nav-tabs nav justify-content-center d-flex" id="myTabs">
+                            <li class="nav-item">
+                                <a href="#conceitos" data-toggle="tab" class="nav-link content-nav">O Que É ...</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#denunciar" data-toggle="tab" class="nav-link content-nav">Como Denunciar</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#leis" data-toggle="tab" class="nav-link content-nav">Crime / Leis</a>
+                            </li>
+                        </ul>
+                        <div class="tab-content">
+                            <div id="conceitos" class="tab-pane container"></div>
+
+                            <div id="denunciar" class="tab-pane container"></div>
+
+                            <div id="leis" class="tab-pane container"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
