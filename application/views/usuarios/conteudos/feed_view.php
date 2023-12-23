6273a635140c793c4361fe1be138b55670745f49
PedroX79 <87133537+pedroD630@users.noreply.github.com>
2023-12-22 22:05:15 -0300

    style: Melhorar o visual dos posts

--- application/views/usuarios/conteudos/feed_view.php
+++ application/views/usuarios/conteudos/feed_view.php
@@ -26,53 +26,15 @@
 
                             <div id="conceitos" class="tab-pane container">
 
-                                
-
                             </div>
 
                             <div id="denunciar" class="tab-pane container">
-                                <div id="adicionar id com titulo do post" class="content-post"><!--div geral do post-->
-                                    <div class="content-post-header"><!--div do cabeçalho com logo e titulo/tag-->
-                                        <div class="logo-post-header">
-                                            <img alt="logo-blaco" id="blaco-logo-post-header"
-                                                src="<?php echo base_url(); ?>utils/img/logo_blaco_white.svg">
-                                        </div>
-                                        <h3 id="content-post-title">Teste</h3>
-                                    </div>
-                                    <div class="post-content"><!--div que contem imagem e texto-->
-                                        <div id="background-content-1" class="content-background">
-                                            <!--div com imagem que preenche o quadrado-->
-                                            <div class="blurred-back-text">
-                                                <div class="blurred-text-header">
-                                                    <h5 class=" subtitulo-post">Subtitulo</h5>
-                                                    <div class="chevron-case"><i class="bi bi-chevron-down"></i></div>
-                                                </div>
-                                                <div class="blurred-text-content">
-                                                    <p>
-                                                        Qualquer distinção, exclusão, restrição ou preferência baseadas
-                                                        em raça, cor,
-                                                        descendência ou origem nacional ou étnica, anulando ou
-                                                        restringindo o reconhecimento,
-                                                        gozo ou exercício de direitos
-                                                        humanos e liberdades fundamentais. (Artigo 1 - Convenção
-                                                        Internacional sobre a
-                                                        Eliminação de todas as Formas de Discriminação Racial)
-                                                    </p>
-                                                </div>
-                                            </div>
-                                            <!--div com texto e fundo borrado que pode ser escondida-->
-                                        </div>
-                                    </div>
-                                    <div class="post-actions">
-                                        <div class="send-action">
-                                            <i class="bi bi-send-fill"></i>
-                                        </div>
-                                    </div>
-
-                                </div>
+                                
                             </div>
 
-                            <div id="leis" class="tab-pane container"></div>
+                            <div id="leis" class="tab-pane container">
+
+                            </div>
                         </div>
                     </div>
                 </div>
